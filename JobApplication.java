08fec9f06ea8f13c3499477e67e3d2cd4e9345b1
Teja Animela <animelateja28@gmail.com>
2025-01-24 11:11:08 +0530

    Update JobApplication.java

--- JobApplication.java
+++ JobApplication.java
@@ -1,33 +1,36 @@
 package kittu;
 
-public class JobApplication {
+public class JobApplication
+    {
     private String companyName;
     private String jobTitle;
     private String applicationDate;
     private String applicationStatus;
-    public JobApplication(String companyName, String jobTitle, String applicationDate, String applicationStatus) {
+    public JobApplication(String companyName, String jobTitle, String applicationDate, String applicationStatus) 
+        {
         this.companyName = companyName;
         this.jobTitle = jobTitle;
         this.applicationDate = applicationDate;
         this.applicationStatus = applicationStatus;
     }
-    public String getCompanyName() {
+    public String getCompanyName() 
+    {
         return companyName;
     }
-
-    public void setCompanyName(String companyName) {
+    public void setCompanyName(String companyName)
+    {
         this.companyName = companyName;
     }
-
-    public String getJobTitle() {
+    public String getJobTitle() 
+    {
         return jobTitle;
     }
-
-    public void setJobTitle(String jobTitle) {
+    public void setJobTitle(String jobTitle)
+    {
         this.jobTitle = jobTitle;
     }
-
-    public String getApplicationDate() {
+    public String getApplicationDate()
+    {
         return applicationDate;
     }
 
@@ -39,12 +42,13 @@ public class JobApplication {
         return applicationStatus;
     }
 
-    public void setApplicationStatus(String applicationStatus) {
+    public void setApplicationStatus(String applicationStatus)
+        {
         this.applicationStatus = applicationStatus;
     }
-
     @Override
-    public String toString() {
+    public String toString()
+        {
         return "Company: " + companyName + ", Job Title: " + jobTitle + ", Date: " + applicationDate + ", Status: " + applicationStatus;
     }
     public static void main(String[] args) {
