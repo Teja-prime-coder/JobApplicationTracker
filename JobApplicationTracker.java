2f78d979f90485e96aa84c27072af14870003504
Teja Animela <animelateja28@gmail.com>
2025-01-24 11:13:49 +0530

    Update JobApplicationTracker.java

--- JobApplicationTracker.java
+++ JobApplicationTracker.java
@@ -3,24 +3,32 @@ package kittu;
 import java.util.ArrayList;
 import java.util.List;
 
-public class JobApplicationTracker {
+public class JobApplicationTracker 
+{
     private List<JobApplication> applications;
-    public JobApplicationTracker() {
+    public JobApplicationTracker()
+    {
         applications = new ArrayList<>();
     }
-    public void addApplication(JobApplication application) {
+    public void addApplication(JobApplication application)
+    {
         applications.add(application);
     }
-    public void printAllApplications() {
-        if (applications.isEmpty()) {
+    public void printAllApplications()
+    {
+        if (applications.isEmpty())
+        {
             System.out.println("No applications yet.");
-        } else {
-            for (JobApplication app : applications) {
+        } else 
+        {
+            for (JobApplication app : applications) 
+            {
                 System.out.println(app);  
             }
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         JobApplicationTracker tracker = new JobApplicationTracker();
         JobApplication application1 = new JobApplication("GOOGLE", "Software Engineer", "2024-12-20", "Applied");
         JobApplication application2 = new JobApplication("AROMA BRAND SOLITIONS", "Software Engineer", "2025-2-7", "Interview Scheduled");
